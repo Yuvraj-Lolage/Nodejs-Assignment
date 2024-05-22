af580d0aae3744f4e12f102f795b14be675e74b0
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-05-22 09:45:55 +0530

    demonstration of NODEJS

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, ()=> { console.log(`App running on http://localhost:${ PORT }`); });
